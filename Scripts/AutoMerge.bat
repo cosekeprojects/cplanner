89c1d2632a9bc0add1c4fb9d62f09baed9ec058b
Eric Watyekele <ewatyeks@gmail.com>
2022-06-23 10:41:37 +0300

    Auto commit Eric Watyekele Coseke Thu 06/23/2022 10:41:37.34

---
+++ Scripts/AutoMerge.bat
@@ -0,0 +1,16 @@
+@echo off
+:TOP
+cd "C:\Users\ewaty\Documents\COSEKE_WORK\REACT\PRT"
+echo "pulling main"
+git pull origin main
+echo "Checking out main branch for cplanner"
+git checkout main
+echo "Merged request for EricDevInitiated"
+git merge EricDev
+echo "Pushing merge request to main"
+git push -u origin main
+echo "Merged Successfully!"
+echo "Returning to EricDev branch!"
+git checkout EricDev
+echo "Now on EricDev branch"
+goto :TOP
