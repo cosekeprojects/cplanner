e2f1a613756e46f4d4f8d948b000f4c9c1e8017c
ericytex <ewatyeks@gmail.com>
2022-07-01 11:55:47 +0300

    autosync2

--- AutoSync.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd "/var/www/reactapp/gdcplanner/cplanner"
-git init
-git checkout main
-git remote add upstream https://github.com/cosekeprojects/cplanner.git
-git fetch upstream
-git merge upstream/main
